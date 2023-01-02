53198047c892a10d31bec7cd91c302829747acb3
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-01-02 21:26:41 +0900

    동전 줍기 대회★2

--- 20230102/main.py
+++ 20230102/main.py
@@ -1,6 +1,6 @@
 ## 2023년 01월 02일 구름 스터디 알고리즘공부
 ## 비타알고 코딩테스트
-## 알파벳 트리 장난감 ★2
+## 동전 줍기 대회★2
 ## https://k-digital.goorm.io/learn/lecture/29413/%EB%B9%84%ED%83%80%EC%95%8C%EA%B3%A0-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%B4%88%EA%B8%89%EB%B6%80%ED%84%B0-%EC%A4%91%EA%B8%89%EA%B9%8C%EC%A7%80/lesson/1454737/%EB%8F%99%EC%A0%84-%EC%A4%8D%EA%B8%B0-%EB%8C%80%ED%9A%8C-2
 
 f = open("20230102/testcase1.txt", 'r')
