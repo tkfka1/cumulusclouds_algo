435e637efb9033b29737341dd61e798a44805a74
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-01-11 16:51:49 +0900

    알파벳 삼각 장난감 ★3

--- 20230110/main.py
+++ 20230110/main.py
@@ -1,90 +1,54 @@
-## 2023년 01월 09일 구름 스터디 알고리즘공부
+## 2023년 01월 10일 구름 스터디 알고리즘공부
 ## 비타알고 코딩테스트
-## 1차원 뿌요뿌요 ★3
-## https://k-digital.goorm.io/learn/lecture/29413/%EB%B9%84%ED%83%80%EC%95%8C%EA%B3%A0-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%B4%88%EA%B8%89%EB%B6%80%ED%84%B0-%EC%A4%91%EA%B8%89%EA%B9%8C%EC%A7%80/lesson/1454746/1%EC%B0%A8%EC%9B%90-%EB%BF%8C%EC%9A%94%EB%BF%8C%EC%9A%94-3
+## 알파벳 삼각 장난감 ★3
+## https://k-digital.goorm.io/learn/lecture/29413/%EB%B9%84%ED%83%80%EC%95%8C%EA%B3%A0-%EC%BD%94%EB%94%A9%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%B4%88%EA%B8%89%EB%B6%80%ED%84%B0-%EC%A4%91%EA%B8%89%EA%B9%8C%EC%A7%80/lesson/1454749/%EC%95%8C%ED%8C%8C%EB%B2%B3-%EC%82%BC%EA%B0%81-%EC%9E%A5%EB%82%9C%EA%B0%90-3
 
-f = open("20230109/testcase1.txt", 'r')
-n,m = map(int,f.readline().split())
-li = list(map(str,f.readline()))
+f = open("20230110/testcase1.txt", 'r')
 
-afterli = []
+length = int(f.readline())
 
-while True:
-	end = li
-	pre = ""
-	stack = 1
-	for i in range(len(li)):
+dic = {}
 
-		if li[i] == pre:
-			stack += 1
-			afterli.append(li[i])
+for i in range(length):
+	dic[i] = list(map(lambda x:ord(x)-64 , f.readline()))
 
+for i in range(1,length):
+	temp = [0]*len(dic[i])
+
+	for key,value in enumerate(dic[i]):
+		if key == 0:
+			temp[key] = dic[i-1][key] + value
 		else:
-			pre = li[i]
-			if stack >= m:
-				for x in range(stack):
-					afterli.pop()
-				afterli += li[i:]
-				break
+			if key+1 == len(dic[i]):
+				temp[key] = dic[i-1][key-1] + value
 			else:
-				afterli.append(li[i])
-
-			stack = 1
-	else:
-		if stack >= m:
-			for x in range(stack):
-				afterli.pop()
+				temp[key] = max(dic[i-1][key-1],dic[i-1][key]) + value
 	
-	li = afterli
-	afterli = []
+	dic[i] = temp
 
-	if li == end:
-		if li:
-			print("".join(li))
-		else:
-			print("CLEAR!")
-		break				
-					
+print(max(dic[length-1]))
 
 ## 구름
 
-# n,m = map(int,input().split())
-# li = list(map(str,input()))
-# afterli = []
+# length = int(input())
 
+# dic = {}
 
-# while True:
-# 	end = li
-# 	pre = ""
-# 	stack = 1
-# 	for i in range(len(li)):
+# for i in range(length):
+# 	dic[i] = list(map(lambda x:ord(x)-64 , input()))
 
-# 		if li[i] == pre:
-# 			stack += 1
-# 			afterli.append(li[i])
+# for i in range(1,length):
+# 	temp = [0]*len(dic[i])
 
+# 	for key,value in enumerate(dic[i]):
+# 		if key == 0:
+# 			temp[key] = dic[i-1][key] + value
 # 		else:
-# 			pre = li[i]
-# 			if stack >= m:
-# 				for x in range(stack):
-# 					afterli.pop()
-# 				afterli += li[i:]
-# 				break
+# 			if key+1 == len(dic[i]):
+# 				temp[key] = dic[i-1][key-1] + value
 # 			else:
-# 				afterli.append(li[i])
-
-# 			stack = 1
-# 	else:
-# 		if stack >= m:
-# 			for x in range(stack):
-# 				afterli.pop()
+# 				temp[key] = max(dic[i-1][key-1],dic[i-1][key]) + value
 	
-# 	li = afterli
-# 	afterli = []
+# 	dic[i] = temp
 
-# 	if li == end:
-# 		if li:
-# 			print("".join(li))
-# 		else:
-# 			print("CLEAR!")
-# 		break
+# print(max(dic[length-1]))
